ed1f6bbfb9088e9a5ddf4cbd53c69f651b5acc0a
unknown <jonaspaq@gmail.com>
2019-12-24 15:28:45 +0800

    Changed assets url to use staging branch files

--- resources/views/includes/footer/production-scripts.blade.php
+++ resources/views/includes/footer/production-scripts.blade.php
@@ -1,3 +1,3 @@
 
-<script src="https://jonaspaq.github.io/vuespa/public/js/app.js"></script>
+<script src="https://jonaspaq.github.io/vuespa/tree/staging/public/js/app.js"></script>
 <script src="https://kit.fontawesome.com/0c7710f8d0.js" crossorigin="anonymous"></script>
--- resources/views/includes/header/production-assets.blade.php
+++ resources/views/includes/header/production-assets.blade.php
@@ -1,7 +1,7 @@
 
 @php 
-    $servedImage = "https://jonaspaq.github.io/vuespa/public/images/hand-peace.png";
-    $servedManifest = "https://jonaspaq.github.io/vuespa/public/manifest.json";
+    $servedImage = "https://jonaspaq.github.io/vuespa/tree/staging/public/images/hand-peace.png";
+    $servedManifest = "https://jonaspaq.github.io/vuespa/tree/staging/public/manifest.json";
 @endphp
 <link rel="icon" href="{{ $servedImage }}" type="image/gif" sizes="32x32">
 <link rel="shortcut icon" href="{{ $servedImage }}">
@@ -10,4 +10,4 @@
 <title> Veza </title>
 
 
-<link rel="stylesheet" href="https://jonaspaq.github.io/vuespa/public/css/app.css">
+<link rel="stylesheet" href="https://jonaspaq.github.io/vuespa/tree/staging/public/css/app.css">

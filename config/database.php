3d7eb33d2e6298e49071f5e1a1ebc41f51b3c4c4
unknown <jonaspaq@gmail.com>
2019-12-24 17:33:45 +0800

    set config to pgqsl

--- config/database.php
+++ config/database.php
@@ -12,6 +12,7 @@ return [
     | Here you may specify which of the database connections below you wish
     | to use as your default connection for all database work. Of course
     | you may use many connections at once using the Database library.
+    | I use PGSQl default to use Heroku free Postgres
     |
     */
 

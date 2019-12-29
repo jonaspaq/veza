5c349312ec7f4db04562d12a4001125232ce6245
unknown <jonaspaq@gmail.com>
2019-12-29 16:18:08 +0800

    Added async attribute to app.js and fontawe kit

--- resources/views/includes/footer/production-scripts.blade.php
+++ resources/views/includes/footer/production-scripts.blade.php
@@ -1,4 +1,4 @@
 
 <!-- <script src="https://jonaspaq.github.io/veza/public/js/app.js"></script> -->
-<script src="{{ secure_asset('js/app.js') }}"></script>
-<script src="https://kit.fontawesome.com/0c7710f8d0.js" crossorigin="anonymous"></script>
+<script async src="{{ secure_asset('js/app.js') }}"></script>
+<script async src="https://kit.fontawesome.com/0c7710f8d0.js" crossorigin="anonymous"></script>

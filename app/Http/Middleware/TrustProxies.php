636c488ac097b8dc125fe3181179b70bee819a36
unknown <jonaspaq@gmail.com>
2019-12-29 13:47:22 +0800

    Test Deploy:

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array|string
      */
-    protected $proxies;
+    protected $proxies = [
+        'https://veza-app.herokuapp.com'
+    ];
 
     /**
      * The headers that should be used to detect proxies.

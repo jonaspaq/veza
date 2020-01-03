0a75093592e118ad558f2276542af6c3f223281d
Jonas <jonaspaq@gmail.com>
2020-01-03 15:58:04 +0800

    Update assets

--- resources/views/includes/footer/production-scripts.blade.php
+++ resources/views/includes/footer/production-scripts.blade.php
@@ -1,5 +1,6 @@
 
 <!-- <script src="https://jonaspaq.github.io/veza/public/js/app.js"></script> -->
-<script async src="{{ secure_asset('js/app.js') }}"></script>
+<!-- <script async src="{{ secure_asset('js/app.js') }}"></script> -->
+<script src="https://veza-app.herokuapp.com/css/app.css"></script>
 <script defer src="https://kit.fontawesome.com/0c7710f8d0.js" crossorigin="anonymous"></script>
 

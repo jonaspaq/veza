4761dced412dc4d57e838c5fb02a439b56a69649
Jonas <jonaspaq@gmail.com>
2020-01-04 00:57:39 +0800

    Set config.yml changes

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mydql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------

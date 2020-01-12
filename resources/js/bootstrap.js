01b9306204e6e4416e1dc864bd6e312a8d9968c8
Jonas <jonaspaq@gmail.com>
2020-01-12 21:08:09 +0800

    Little changes for laravel echo settings

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -56,8 +56,8 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.PUSHER_APP_KEY,
-    cluster: process.env.PUSHER_APP_CLUSTER,
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     encrypted: true,
     authEndpoint: '/broadcasting/auth',
     auth:{

7cd36c9ab950b951da44fa90d4ac784292e599b7
Jonas <jonaspaq@gmail.com>
2020-01-12 21:47:18 +0800

    Remove unused migration

--- database/migrations/2019_11_07_093857_create_jobs_table.php
+++ database/migrations/2019_11_07_093857_create_jobs_table.php
@@ -13,15 +13,15 @@ class CreateJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('jobs', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('queue')->index();
-            $table->longText('payload');
-            $table->unsignedTinyInteger('attempts');
-            $table->unsignedInteger('reserved_at')->nullable();
-            $table->unsignedInteger('available_at');
-            $table->unsignedInteger('created_at');
-        });
+        // Schema::create('jobs', function (Blueprint $table) {
+        //     $table->bigIncrements('id');
+        //     $table->string('queue')->index();
+        //     $table->longText('payload');
+        //     $table->unsignedTinyInteger('attempts');
+        //     $table->unsignedInteger('reserved_at')->nullable();
+        //     $table->unsignedInteger('available_at');
+        //     $table->unsignedInteger('created_at');
+        // });
     }
 
     /**
@@ -31,6 +31,6 @@ class CreateJobsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('jobs');
+        // Schema::dropIfExists('jobs');
     }
 }

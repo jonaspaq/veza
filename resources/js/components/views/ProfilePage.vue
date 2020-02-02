ff55484efcf95a1093d4ed498e6bb045ccb1d0f4
Jonas <jonaspaq@gmail.com>
2020-02-02 21:43:42 +0800

    Added profile page

---
+++ resources/js/components/views/ProfilePage.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container-fluid">
+        <div class="row profile-cover align-items-center">
+            <div class="col-12">
+                <h2 class="text-white text-center">{{ user.name }}</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProfilePage',
+
+    computed:{
+        user(){
+            return this.$store.getters['auth/user']
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.profile-cover{
+    background: var(--primary-color-linear);
+    height: 30vh;
+    padding: 0;
+}
+</style>
